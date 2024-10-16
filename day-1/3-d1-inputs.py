cac609a7561662dd822ff71c8695ba263ab4c062
abdhfz93 <abd.hfz93@gmail.com>
2024-10-16 21:29:53 +0800

    day1.1

---
+++ day-1/3-d1-inputs.py
@@ -0,0 +1,5 @@
+#your_name=input("What is your name?")
+#print("Your name is "+your_name)
+#print("Hello "+input("What is your actual name?"))
+#inside bracket prio
+print("Kami Sayang "+input("What is your child name?")+"!")
---
+++ day-1/4-d1-variable.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+your_baby = input("What is your baby's name?")
+print("Ok, your child fullname is "+your_baby+" binti "+name)
+print(len(name+your_baby))
+print(len(your_baby))
+
+print(len(input("ur name pls")))
