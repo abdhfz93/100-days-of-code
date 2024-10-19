5c5b66eba0b3b8ff433f390b92d633d7c4f8c064
abdhfz93 <abd.hfz93@gmail.com>
2024-10-19 22:23:03 +0800

    D3

--- day-2/4-number-manipulation.py
+++ day-2/4-number-manipulation.py
@@ -1,1 +1,20 @@
-print("Hello world!\nHello world!\nHello world!")
+bmi = 84 / 1.65 ** 2
+
+print(bmi)
+
+print(int(bmi))
+
+print(round(bmi)) #accurately round to whole number
+
+print(round(bmi,2)) #2 decimal places
+
+score1 = 0
+score1 +=1
+print(score1)
+
+score2 = 0
+score2 -=1
+print(score2)
+
+#f-string
+print(f"your score is : {score1}, your friend's score is : {score2}")
--- day-2/5-tip-calculator.py
+++ day-2/5-tip-calculator.py
@@ -1,1 +1,12 @@
-print("Hello world!\nHello world!\nHello world!")
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15? "))
+people = int(input("How many people to split the bill? "))
+bill_w_tip=(bill*tip/100)+bill
+after_split=bill_w_tip/people
+final_amt=round(after_split,2)
+
+print(f"Each person should pay: ${final_amt}")
+
+
+
