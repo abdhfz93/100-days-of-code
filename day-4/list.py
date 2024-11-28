fa57b228d5da8dcc5224956b8a441fe4177871ee
abdhfz93 <abd.hfz93@gmail.com>
2024-11-28 10:27:47 +0800

    d4

---
+++ day-4/list.py
@@ -0,0 +1,10 @@
+fruits = ["item1"",item2"]
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+states_of_malaysia = ["Selangor","Johor","Kedah","Kelantan","Pahang","Perak","Melaka"]
+states_of_malaysia[5]="Kelate"
+states_of_malaysia.append("Perlis")
+print(states_of_america[0]+", " + states_of_malaysia[5])
+
+
+
