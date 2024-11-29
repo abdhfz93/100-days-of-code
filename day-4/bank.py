b21e59044aef94af58124c9cf6254326b6e8393a
abdhfz93 <abd.hfz93@gmail.com>
2024-11-29 14:29:26 +0800

    d4

---
+++ day-4/bank.py
@@ -0,0 +1,11 @@
+import random
+
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+#opt 1 previous knowledge
+test=random.randint(0,4)
+print(friends[test])
+b=(friends[test])
+#opt 2 , by googling
+print(random.choice(friends))
+a=(random.choice(friends))
